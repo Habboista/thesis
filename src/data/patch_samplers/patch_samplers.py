63e940108da2a7f88d934c687068a1f721132853
Habboista <antonio.fusillo99@gmail.com>
2024-07-15 16:06:46 +0200

    Completed the sampling procedure. Things to do next: improve the sampling since it is inefficient right now; sample only areas with enough depth points in-view; handle out-of-vew points when sampling and warping.

--- src/data/patch_samplers/patch_samplers.py
+++ src/data/patch_samplers/patch_samplers.py
@@ -1,6 +1,7 @@
 import random
 
 import numpy as np
+import skimage
 import torch
 from torch import Tensor
 import torchvision.transforms.functional as F
@@ -20,35 +21,89 @@ class NoPatchSampler(PatchSampler):
 
         return image.unsqueeze(0), depth_map.unsqueeze(0)
 
-class RotatedPatchSampler(PatchSampler):
-    def __init__(self, batch_size: int):
+class MyPatchSampler(PatchSampler):
+    def __init__(
+            self,
+            batch_size: int,
+            blur: bool,
+            half_h: int,
+            half_w: int,
+            corner_sampling: bool,
+        ):
+        self.half_h = half_h
+        self.half_w = half_w
         self.batch_size = batch_size
+        self.blur = blur
+        self.corner_sampling = corner_sampling
 
     def _call(self, image: Tensor, point_cloud: PointCloud) -> tuple[Tensor, Tensor]:
-        samples = [self.sample(image, point_cloud) for _ in range(self.batch_size)]
+        print("image shape before crop",image.shape[-2:])
 
-        image_crops = [s[0] for s in samples]
-        depth_map_crops = [s[1] for s in samples]
+        # Sample points of interest and warp the image and depth centering them
+        sample = self.random_sampling if not self.corner_sampling else self.sample_corner
+        samples = [sample(image, point_cloud) for _ in range(self.batch_size)]
+
+        # Crop around the central point of the image (as defined by the camera)
+        image_crops = [s[0]\
+            [...,
+            int(point_cloud.camera_info['K'][1, 2] - self.half_h):int(point_cloud.camera_info['K'][1, 2] + self.half_h),
+            int(point_cloud.camera_info['K'][0, 2] - self.half_w):int(point_cloud.camera_info['K'][0, 2] + self.half_w)]
+            for s in samples]
+        depth_map_crops = [s[1]\
+            [...,
+            int(point_cloud.camera_info['K'][1, 2] - self.half_h):int(point_cloud.camera_info['K'][1, 2] + self.half_h),
+            int(point_cloud.camera_info['K'][0, 2] - self.half_w):int(point_cloud.camera_info['K'][0, 2] + self.half_w)]
+            for s in samples]
 
         # Batch crops
         image = torch.stack(image_crops)
         depth_map = torch.stack(depth_map_crops)
 
-        return image, depth_map
+        return blur(image) if self.blur else image, depth_map
 
-    def sample(self, image: Tensor, point_cloud: PointCloud) -> tuple[Tensor, Tensor]:
-        # Randomly select center of image
-        p1 = 0.1
-        p2 = 0.9
+    def random_sampling(self, image: Tensor, point_cloud: PointCloud) -> tuple[Tensor, Tensor]:
+        # Randomly select point
+        p1 = 0.2 # TODO: p1, p2 should depend on image size
+        p2 = 0.8
         x = random.randrange(int(p1 * image.shape[-1]), int(p2 * image.shape[-1]))
         y = random.randrange(int(p1 * image.shape[-2]), int(p2 * image.shape[-2]))
 
         return self.warp(image, point_cloud, x, y)
     
+    def sample_corner(self, image: Tensor, point_cloud: PointCloud) -> tuple[Tensor, Tensor]:
+        assert len(image.shape) == 3
+        assert image.shape[0] == 3
+
+        np_image = image.permute(1, 2, 0).cpu().numpy()
+        np_corner_response = skimage.feature.corner_moravec(skimage.color.rgb2gray(np_image))
+
+        # Cancel response near image boundaries (otherwise the warp contains out-of-view points)
+        p1 = 0.2
+        p2 = 0.8
+        i1 = int(p1 * image.shape[-1])
+        i2 = int(p2 * image.shape[-1])
+        j1 = int(p1 * image.shape[-2])
+        j2 = int(p2 * image.shape[-2]) 
+        np_corner_response[:i1, :] = 0
+        np_corner_response[i2:, :] = 0
+        np_corner_response[:, :j1] = 0
+        np_corner_response[:, j2:] = 0
+        peaks = skimage.feature.corner_peaks(np_corner_response) # TODO: this can be empty
+        print(peaks.shape)
+        print(np_corner_response.min(), np_corner_response.max())
+        i = random.choice(range(peaks.shape[0]))
+        x = peaks[i, 0]
+        y = peaks[i, 1]
+
+        return self.warp(image, point_cloud, x, y)
+    
     def warp(self, image: Tensor, point_cloud: PointCloud, x: int, y: int) -> tuple[Tensor, Tensor]:
+        """Given a point (x, y) applies perspective transform to both the image and the point cloud
+        so that the point matches the central point (defined by px, py parameters of the camera) of the image."""
+        
         # Compute angles of rotation
-        alpha_x = point_cloud.camera_info['K'][0, 0]
-        alpha_y = point_cloud.camera_info['K'][1, 1]
+        alpha_x = abs(point_cloud.camera_info['K'][0, 0])
+        alpha_y = abs(point_cloud.camera_info['K'][1, 1])
         py = int(point_cloud.camera_info['K'][1, 2])
         px = int(point_cloud.camera_info['K'][0, 2])
         theta_yz = -np.arctan2(y - py, alpha_y)
@@ -68,7 +123,9 @@ class RotatedPatchSampler(PatchSampler):
             [0,                 0,                0, 1],
         ])
 
-        # Points in space for computing transformation
+        # Arbitrary points in space for computing the homography
+        # They just need to be a homogeneous reference system
+        # i.e. each three of them is a group of independent vectors
         offset = 1
         corners = np.array([
             [-offset, -offset, offset*10, 1],
@@ -76,9 +133,11 @@ class RotatedPatchSampler(PatchSampler):
             [ offset, -offset, offset*10, 1],
             [ offset,  offset, offset*10, 1],
         ])
+
+        # Camera matrix (3x4)
         P = np.hstack((point_cloud.camera_info['K'], np.zeros((3, 1))))
 
-        # Points projected in transformed image
+        # Points projected in warped image
         end_points = corners @ P.T
         end_points[:, :2] /= end_points[:, 2:]
         end_points = end_points[:, :2]
